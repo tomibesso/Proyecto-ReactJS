6f2c5869d87b9a3da5f37eac87838f293af5828b
Tomas Besso <tomi.besso12@gmail.com>
2023-12-02 10:20:45 -0300

    agregar nombre de la tienda

--- src/components/NavBarComponent/NavBarComponent.jsx
+++ src/components/NavBarComponent/NavBarComponent.jsx
@@ -8,7 +8,7 @@ function BasicExample() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Vivir El Verde</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
